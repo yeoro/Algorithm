c76fcc473361a119a04fca0f2f30cccad1231f38
yeoro <49011586+yeoro@users.noreply.github.com>
2021-01-07 03:09:42 +0900

    20210107 | BOJ 2741 N 찍기

---
+++ Solutions/src/BOJ_02741_N찍기/BOJ_02741_N찍기.java
@@ -0,0 +1,20 @@
+package BOJ_02741_NÂï±â;
+
+import java.util.Scanner;
+
+public class BOJ_02741_NÂï±â {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ Solutions/src/BOJ_02741_N찍기/README.md
@@ -0,0 +1,11 @@
+# <img src="https://static.solved.ac/tier_small/3.svg" width=30> 2741: N 찍기
+
+### 문제 링크
+> https://www.acmicpc.net/problem/2741
+
+### 알고리즘 분류
+>- 구현
+
+### 풀이 시간 및 해결
+>- ⏱ 30초
+>- 반복문 돌며 출력
