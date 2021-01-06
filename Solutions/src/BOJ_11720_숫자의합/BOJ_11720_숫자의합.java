a1bc33d1c806170039c900686bb35d8b38d754db
yeoro <49011586+yeoro@users.noreply.github.com>
2021-01-07 02:10:05 +0900

    20210107 | BOJ 11720 숮자의 합

---
+++ Solutions/src/BOJ_11720_숫자의합/BOJ_11720_숫자의합.java
@@ -0,0 +1,27 @@
+package BOJ_11720_숫자의합;
+
+import java.util.Scanner;
+
+public class BOJ_11720_숫자의합 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		String s = sc.next();
+		
+		int sum = 0;
+		
+		for(int i = 0; i < num; i++) {
+			
+			sum += Integer.parseInt(s.charAt(i) + "");
+		}
+		
+		System.out.println(sum);
+		
+		sc.close();
+	}
+
+}
---
+++ Solutions/src/BOJ_11720_숫자의합/README.md
@@ -0,0 +1,13 @@
+# <img src="https://static.solved.ac/tier_small/4.svg" width=30> 11720: 숫자의 합
+
+### 문제 링크
+> https://www.acmicpc.net/problem/11720
+
+### 알고리즘 분류
+>- 수학
+>- 문자열
+>- 사칙연산
+
+### 풀이 시간 및 해결
+>- ⏱ 8분
+>- 한 글자씩 잘라 파싱 후 합을 구하여 출력
