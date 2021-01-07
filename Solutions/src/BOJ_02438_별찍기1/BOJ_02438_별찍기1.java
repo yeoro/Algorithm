6a59e39b84669e64dd23db8170bc29df6f9eba29
yeoro <49011586+yeoro@users.noreply.github.com>
2021-01-07 17:27:05 +0900

    20210107 | BOJ 2438 별 찍기 - 1

---
+++ Solutions/src/BOJ_02438_별찍기1/BOJ_02438_별찍기1.java
@@ -0,0 +1,21 @@
+package BOJ_02438_º°Âï±â1;
+
+import java.util.Scanner;
+
+public class BOJ_02438_º°Âï±â1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		
+		for(int tc = 0; tc < TC; tc++) {
+			for(int i = 0; i <= tc; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
---
+++ Solutions/src/BOJ_02438_별찍기1/README.md
@@ -0,0 +1,12 @@
+# <img src="https://static.solved.ac/tier_small/3.svg" width=30> 2438: 별 찍기 - 1
+
+### 문제 링크
+> https://www.acmicpc.net/problem/2438
+
+### 알고리즘 분류
+>- 구현
+>- 문자열
+
+### 풀이 시간 및 해결
+> - ⏱ 30초
+> - 이중 반복문 사용
