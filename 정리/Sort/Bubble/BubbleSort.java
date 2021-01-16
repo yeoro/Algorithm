9e02b28a82e50fb0f4e350947d88da4e9484cba7
yeoro <dufgh1009@naver.com>
2021-01-16 14:49:44 +0900

    create Folder & Burbble Sort

---
+++ 정리/Sort/Bubble/BubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+
+		int[] nums = {1000, 400, 12, -59, 328, 121, -3};
+		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < nums.length-1; j++) {
+				if(nums[j] > nums[j+1]) {
+					int temp = nums[j];
+					nums[j] = nums[j+1];
+					nums[j+1] = temp;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(nums));
+	}
+}
---
+++ 정리/Sort/README.md
@@ -0,0 +1,5 @@
+# 정렬
+- 주어진 데이터를 숫자의 순서나 어휘의 순서대로 나열하는 것  
+- 현재 비교정렬 알고리즘은 시간복잡도 O(nlogn) 이하로 줄일 수 없다고 증명됨
+- 정렬 알고리즘별 시간, 공간 복잡도 비교
+    ![Comparison_of_Sorting_Algorithm](https://s3.ap-south-1.amazonaws.com/afteracademy-server-uploads/comparison-of-sorting-algorithms-compare1-18082c14f960abf3.png)
