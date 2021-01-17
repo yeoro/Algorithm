1014ee5ce77425f1c0f0f239643061db31fcfa58
yeoro <dufgh1009@naver.com>
2021-01-18 01:06:07 +0900

    20210117 | BOJ 11650 좌표 정렬하기

---
+++ Solutions/src/BOJ_11650_좌표정렬하기/BOJ_11650_좌표정렬하기.java
@@ -0,0 +1,49 @@
+package BOJ_11650_좌표정렬하기;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ_11650_좌표정렬하기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		ArrayList<coordinates> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(new coordinates(sc.nextInt(), sc.nextInt()));
+		}
+		
+		Collections.sort(list);
+		
+		for(coordinates c : list) {
+			System.out.println(c.x + " " + c.y);
+		}
+		
+		sc.close();
+	}
+	
+}
+
+class coordinates implements Comparable<coordinates> {
+	int x;
+	int y;
+	
+	public coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(coordinates o) {
+		if(this.x - o.x == 0) {
+			return this.y - o.y;
+		}
+		return this.x - o.x;
+	}
+}
+
---
+++ Solutions/src/BOJ_11650_좌표정렬하기/README.md
@@ -0,0 +1,15 @@
+# <img src="https://static.solved.ac/tier_small/6.svg" width=30> 11650: 좌표 정렬하기
+
+### 문제 링크
+> https://www.acmicpc.net/problem/11650
+
+### 알고리즘 분류
+>- 정렬
+
+### 풀이 시간
+>- ⏱ 17분
+
+### 해결
+> 1. 2개의 좌표(x, y)를 갖는 클래스 생성
+> 2. Comparable 인터페이스 상속 후 CompareTo 메소드 오버라이딩
+> 3. x의 값이 같으면 y를 오름차순 정렬하는 조건 추가
