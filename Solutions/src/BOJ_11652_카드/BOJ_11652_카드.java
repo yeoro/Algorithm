b05d6279b30cd8f6dd3981c300313441975de8d5
yeoro <dufgh1009@naver.com>
2021-01-19 21:02:56 +0900

    20210119 | BOJ 11652 카드

---
+++ Solutions/src/BOJ_11652_카드/BOJ_11652_카드.java
@@ -0,0 +1,45 @@
+package BOJ_11652_카드;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ_11652_카드 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		ArrayList<Long> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(sc.nextLong());
+		}
+		
+		Collections.sort(list);
+
+		int cnt = 1;
+		int maxCnt = 1;
+		Long maxNum = list.get(0);
+		
+		for(int i = 1; i < list.size(); i++) {
+			if(list.get(i-1) == list.get(i)) {
+				cnt++;
+			} else {
+				cnt = 1;
+			}
+			
+			if(maxCnt < cnt) {
+				maxCnt = cnt;
+				maxNum = list.get(i);
+			}
+		}
+		
+		System.out.println(maxNum);
+		
+		sc.close();
+	}
+}
