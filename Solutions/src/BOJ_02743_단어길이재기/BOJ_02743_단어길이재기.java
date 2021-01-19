5cfab660535ec4298ca452fef8268f62d924d37e
yeoro <dufgh1009@naver.com>
2021-01-19 23:46:33 +0900

    20210119 | BOJ 2743 단어 길이 재기

---
+++ Solutions/src/BOJ_02743_단어길이재기/BOJ_02743_단어길이재기.java
@@ -0,0 +1,18 @@
+package BOJ_02743_단어길이재기;
+
+import java.util.Scanner;
+
+public class BOJ_02743_단어길이재기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		System.out.println(s.length());
+		
+		sc.close();
+		
+	}
+}
---
+++ Solutions/src/BOJ_02743_단어길이재기/README.md
@@ -0,0 +1,14 @@
+# <img src="https://static.solved.ac/tier_small/4.svg" width=30> 2743: 단어 길이 재기
+
+### 문제 링크
+> https://www.acmicpc.net/problem/2743
+
+### 알고리즘 분류
+>- 구현
+>- 문자열
+
+### 풀이 시간
+>- ⏱ 1분
+
+### 해결
+> 1. **length()** 메소드 이용
