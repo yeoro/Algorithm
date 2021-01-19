113d8868056e8d77856d165d34a72b02867cef71
yeoro <dufgh1009@naver.com>
2021-01-19 21:23:10 +0900

    20210119 | BOJ 11004 K번째 수

--- Solutions/src/BOJ_11004_K번째수/BOJ_11004_K번째수.java
+++ Solutions/src/BOJ_11004_K번째수/BOJ_11004_K번째수.java
@@ -1,45 +1,41 @@
 package BOJ_11004_K번째수;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class BOJ_11004_K번째수 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s, " ");
 		
-		ArrayList<Long> list = new ArrayList<>();
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		for(int i = 0; i < N; i++) {
-			list.add(sc.nextLong());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		s = br.readLine();
+		st = new StringTokenizer(s, " ");
+		
+		while(st.hasMoreTokens()) {
+			list.add(Integer.parseInt(st.nextToken()));
 		}
 		
 		Collections.sort(list);
+		
+		System.out.println(list.get(K-1));
 
-		int cnt = 1;
-		int maxCnt = 1;
-		Long maxNum = list.get(0);
-		
-		for(int i = 1; i < list.size(); i++) {
-			if(list.get(i-1).equals(list.get(i))) {
-				cnt++;
-			} else {
-				cnt = 1;
-			}
-			
-			if(maxCnt < cnt) {
-				maxCnt = cnt;
-				maxNum = list.get(i);
-			}
-		}
-		
-		System.out.println(maxNum);
-		
-		sc.close();
+		br.close();
 	}
 }
--- Solutions/src/BOJ_11004_K번째수/README.md
+++ Solutions/src/BOJ_11004_K번째수/README.md
@@ -1,17 +1,17 @@
-# <img src="https://static.solved.ac/tier_small/7.svg" width=30> 11652: 카드
+# <img src="https://static.solved.ac/tier_small/7.svg" width=30> 11004: K번째 수
 
 ### 문제 링크
-> https://www.acmicpc.net/problem/11652
+> https://www.acmicpc.net/problem/11004
 
 ### 알고리즘 분류
 >- 정렬
 
 ### 풀이 시간
->- ⏱ 30분+
+>- ⏱ 15분
 
 ### 해결
-> 1. Long 타입의 ArrayList에 입력된 수 저장
-> 2. 1부터 리스트의 크기만큼 반복문
-> 3. 이전 인덱스와 값이 같으면 카운트 1증가, 값이 다르면 카운트 1로 초기화
-> 4. 카운트가 현재 최대 카운트보다 크면 현재 카운트를 최대 카운트로 설정
-> 5. 현재 인덱스의 값을 최빈값으로 설정
+> 1. ArrayList 선언 후 입력 값 저장
+> 2. Collections.sort 로 리스트 정렬 후 K번째 값 출력
+>- Scanner는 시간초과
+>- BufferedReader + StringTokenizer
+>- 어차피 StringTokenizer 개수 만큼 while문을 돌기 때문에 N 값을 버려도 될듯..?
