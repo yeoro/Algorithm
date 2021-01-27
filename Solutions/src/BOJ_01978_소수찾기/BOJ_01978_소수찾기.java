ddccb1ac29e469aa95b52f7063b4250890b8a826
yeoro <dufgh1009@naver.com>
2021-01-27 22:00:43 +0900

    20210127 | BOJ 1978 소수 찾기

---
+++ Solutions/src/BOJ_01978_소수찾기/BOJ_01978_소수찾기.java
@@ -0,0 +1,36 @@
+package BOJ_01978_소수찾기;
+
+import java.util.Scanner;
+
+public class BOJ_01978_소수찾기 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int prime = 0;
+		
+		for(int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			boolean isPrime = true;
+			
+			if(num > 1) {
+				for(int j = 2; j < num; j++) {
+					if(num % j == 0) {
+						isPrime = false;
+						break;
+					}
+				}
+				
+				if(isPrime) {
+					prime++;
+				}
+			}
+		}
+		
+		System.out.println(prime);
+		sc.close();
+	}
+}
+
