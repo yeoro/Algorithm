8fb1a4ff6e552f7af36e24cd5a4df52cd09e0219
yeoro <dufgh1009@naver.com>
2021-01-31 23:49:42 +0900

    20210131 | BOJ 1001 A - B

---
+++ Solutions/src/BOJ_01001_AB/BOJ_01001_AB.java
@@ -0,0 +1,19 @@
+package BOJ_01001_AB;
+
+import java.util.Scanner;
+
+public class BOJ_01001_AB {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A-B);
+		
+		sc.close();
+	}
+	
+}
---
+++ Solutions/src/BOJ_01001_AB/README.md
@@ -0,0 +1,13 @@
+# <img src="https://static.solved.ac/tier_small/1.svg" width=30> 1001: A - B
+
+### 문제 링크
+> https://www.acmicpc.net/problem/1001
+
+### 알고리즘 분류
+>- 수학
+>- 구현
+>- 사칙연산
+
+### 풀이 시간 및 해결
+> - ⏱ 20초
+> - 두 정수 A와 B를 입력 받은 후 차를 출력
