00c2fbdcdf12b9a1b18725a4d1fd0aac1cf75fce
yeoro <dufgh1009@naver.com>
2021-02-02 23:44:53 +0900

    20210202 | BOJ 15651 N과 M (3)

---
+++ Solutions/src/BOJ_15651_N과M3/BOJ_15651_N과M3.java
@@ -0,0 +1,52 @@
+package BOJ_15651_N°úM3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_15651_N°úM3 {
+	
+	static int N, M;
+	static int[] input, output;
+	static BufferedWriter bw;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N+1];
+		output = new int[M+1];
+		
+		for(int i = 1; i <= N; i++) {
+			input[i] = i;
+		}
+		
+		permutation(0);
+		
+		br.close();
+		bw.flush();
+	}	
+	
+	private static void permutation(int depth) throws IOException {
+		if(depth == M) {
+			for(int i = 1; i <= M; i++) {
+				bw.write(output[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			output[depth+1] = input[i];
+			permutation(depth+1);
+		}
+	}
+}
+
---
+++ Solutions/src/BOJ_15651_N과M3/README.md
@@ -0,0 +1,16 @@
+# <img src="https://static.solved.ac/tier_small/8.svg" width=30> 15651: N과 M (3)
+
+### 문제 링크
+
+> https://www.acmicpc.net/problem/15651
+
+### 알고리즘 분류
+>- 백트래킹
+
+### 풀이 시간
+
+> - ⏱ 6분
+
+### 해결
+
+> 1. 방문체크 없이 순열 생성
