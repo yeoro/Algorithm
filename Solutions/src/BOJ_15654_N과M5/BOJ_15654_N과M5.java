4eb12d214dc90eaa95595b9fb08e1c6ac1097aa9
yeoro <dufgh1009@naver.com>
2021-02-03 21:26:48 +0900

    20210203 | BOJ 15654 N과 M (5)

---
+++ Solutions/src/BOJ_15654_N과M5/BOJ_15654_N과M5.java
@@ -0,0 +1,67 @@
+package BOJ_15654_N°úM5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15654_N°úM5 {
+
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M;
+	static int[] input, output;
+	static boolean[] v;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N+1];
+		v = new boolean[N+1];
+		output = new int[M+1];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int i = 1;
+		while(st.hasMoreTokens()) {
+			input[i] = Integer.parseInt(st.nextToken());
+			++i;
+		}
+		
+		Arrays.sort(input);
+		
+		permutation(0);
+		
+		br.close();
+		bw.flush();
+	}
+	
+	private static void permutation(int depth) throws IOException {
+		if(depth == M) {
+			for(int i = 1; i <= M; i++) {
+				bw.write(output[i] + " ");
+			}
+			bw.write("\n");
+			return;
+			
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(v[i]) {
+				continue;
+			}
+			
+			v[i] = true;
+			output[depth+1] = input[i];
+			permutation(depth+1);
+			v[i] = false;
+		}
+	}
+}
+
---
+++ Solutions/src/BOJ_15654_N과M5/README.md
@@ -0,0 +1,17 @@
+# <img src="https://static.solved.ac/tier_small/8.svg" width=30> 15654: N과 M (5)
+
+### 문제 링크
+
+> https://www.acmicpc.net/problem/15654
+
+### 알고리즘 분류
+>- 백트래킹
+
+### 풀이 시간
+
+> - ⏱ 11분
+
+### 해결
+
+> 1. 방문체크 배열 사용
+> 2. **BufferedReader**, **BufferedWriter** 사용
