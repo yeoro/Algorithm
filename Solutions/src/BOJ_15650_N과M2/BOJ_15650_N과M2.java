209df964be8c8656033a752825bd2d22d305ab87
yeoro <dufgh1009@naver.com>
2021-02-03 19:22:58 +0900

    20210203 | BOJ 15650 N과 M (2)

---
+++ Solutions/src/BOJ_15650_N과M2/BOJ_15650_N과M2.java
@@ -0,0 +1,62 @@
+package BOJ_15650_N°úM2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_15650_N°úM2 {
+	
+	static int[] input, output;
+	static boolean[] v;
+	static int N, M;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N+1];
+		v = new boolean[N+1];
+		output = new int[M+1];
+		
+		
+		for(int i = 1; i <= N; i++) {
+			input[i] = i;
+		}
+		
+		permutation(0);
+		
+		br.close();
+		bw.flush();
+	}
+	
+	private static void permutation(int depth) throws IOException {
+		if(depth == M) {
+			for(int i = 1; i <= M; i++) {
+				bw.write(output[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(v[i]) {
+				continue;
+			}
+			
+			if(output[depth] < input[i]) {
+				v[i] = true;
+				output[depth+1] = input[i];
+				permutation(depth+1);
+				v[i] = false;
+			}
+		}
+	}
+}
+
---
+++ Solutions/src/BOJ_15650_N과M2/README.md
@@ -0,0 +1,18 @@
+# <img src="https://static.solved.ac/tier_small/8.svg" width=30> 15650: N과 M (2)
+
+### 문제 링크
+
+> https://www.acmicpc.net/problem/15650
+
+### 알고리즘 분류
+>- 백트래킹
+
+### 풀이 시간
+
+> - ⏱ 6분
+
+### 해결
+
+> 1. 방문체크 배열 사용
+> 2. 다음 수가 클 경우에만 순열 생성
+> 2. **BufferedReader**, **BufferedWriter** 사용
