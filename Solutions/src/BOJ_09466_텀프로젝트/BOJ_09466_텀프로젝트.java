24c793ba087dea1efbf7ab11a2bba368236524c7
yeoro <dufgh1009@naver.com>
2021-02-19 19:01:09 +0900

    20210219 | BOJ 9466 텀 프로젝트

--- Solutions/src/BOJ_09466_텀프로젝트/BOJ_09466_텀프로젝트.java
+++ Solutions/src/BOJ_09466_텀프로젝트/BOJ_09466_텀프로젝트.java
@@ -1,17 +1,15 @@
 package BOJ_09466_텀프로젝트;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 public class BOJ_09466_텀프로젝트 {
 	
 	static LinkedList<Integer> adj[];
-	static boolean[] v;
+	static ArrayList<Integer> check;
+	static boolean[] v, cycle;
 	static int n, cnt;
+	static boolean isCycle;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +24,7 @@ public class BOJ_09466_텀프로젝트 {
 			n = Integer.parseInt(st.nextToken());
 			adj = new LinkedList[n+1];
 			v = new boolean[n+1];
+			cycle = new boolean[n+1];	
 			cnt = 0;
 			
 			st = new StringTokenizer(br.readLine(), " ");
@@ -38,9 +37,17 @@ public class BOJ_09466_텀프로젝트 {
 			}
 
 			for(int i = 1; i <= n; i++) {
-				if(!v[i]) {
+				check = new ArrayList<Integer>();
+				isCycle = false;
+				if(!cycle[i]) {
 					v[i] = true;
-					dfs(i, i, 1);
+					check.add(i);
+					dfs(i, i);
+				}
+				if(isCycle) {
+					for(int c : check) {
+						cycle[c] = true;
+					}
 				}
 			}
 			
@@ -51,24 +58,33 @@ public class BOJ_09466_텀프로젝트 {
 		bw.flush();
 	}
 	
-	private static void dfs(int node, int start, int depth) {
+	private static void dfs(int node, int start) {
 		ListIterator<Integer> it = adj[node].listIterator();
 		
 		while(it.hasNext()) {
 			int i = it.next();
-			if(i == start) {
-//				System.out.println("다음: " + i + " 시작점: " + start + " 선택: " + depth); 
-//				System.out.println(Arrays.toString(v));
-				cnt += depth;
+			
+			if(i == node) { // 자기 자신 선택
+				cnt++;
+				cycle[i] = true;
+				v[i] = true;
+				isCycle = true;
+				return;
+			}
+			
+			if(i == start) { // 다른 사람 선택
+				cnt += check.size();
+				isCycle = true;
+				return;
 			}
 
 			if(!v[i]) {
 				v[i] = true;
-				
-//				System.out.println("현재: " + node + " 다음: " + i);
-				
-				dfs(i, start, depth+1);
-				v[i] = false;
+				check.add(i);
+				dfs(i, start);
+				if(!cycle[i]) {
+					v[i] = false;
+				}
 			}
 		}
 	}
