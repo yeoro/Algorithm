5942db8c7374ac266f147a24a68f35f6aae5fd8a
yeoro <dufgh1009@naver.com>
2021-02-23 17:54:22 +0900

    20210223 | BOJ 11403 경로 찾기

--- Solutions/src/BOJ_11403_경로찾기/BOJ_11403_경로찾기.java
+++ Solutions/src/BOJ_11403_경로찾기/BOJ_11403_경로찾기.java
@@ -5,9 +5,7 @@ import java.util.*;
 
 public class BOJ_11403_경로찾기 {
 
-	static int[][] result;
-	static LinkedList<Integer> adj[];
-	static boolean[] v;
+	static int[][] adj;
 	static int N;
 	
 	public static void main(String[] args) throws IOException {
@@ -17,51 +15,34 @@ public class BOJ_11403_경로찾기 {
 		
 		N = Integer.parseInt(br.readLine());
 		
-		adj = new LinkedList[N];
-		v = new boolean[N];
-		result = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			adj[i] = new LinkedList<Integer>();
-		}
+		adj = new int[N][N];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j = 0; j < N; j++) {
-				int edge = Integer.parseInt(st.nextToken());
-				
-				if(edge == 1) {
-					adj[i].add(j);
+				adj[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(adj[i][k] == 1 && adj[k][j] == 1) {
+						adj[i][j] = 1;
+					}
 				}
 			}
 		}
 		
 		for(int i = 0; i < N; i++) {
-			dfs(i, i);
+			for(int j = 0; j < N; j++) {
+				bw.write(adj[i][j] + " ");
+			}
+			bw.write("\n");
 		}
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				System.out.print(result[i][j] + " ");
-			}
-			System.out.println();
-		}
 		br.close();
 		bw.flush();
-	}
-	
-	private static void dfs(int node, int root) {
-		for(int i : adj[node]) {
-			if(result[node][i] == 0) {
-				result[node][i] = 1;
-			}
-			
-			if(result[root][i] == 0) {
-				result[root][i] = 1;
-			}
-			
-			dfs(i, root);
-		}
 	}
 }
 
--- Solutions/src/BOJ_11403_경로찾기/README.md
+++ Solutions/src/BOJ_11403_경로찾기/README.md
@@ -1,13 +1,14 @@
-# <img src="https://static.solved.ac/tier_small/31.svg" width=30> 00000: 
+# <img src="https://static.solved.ac/tier_small/10.svg" width=30> 11403: 경로 찾기 
 
 ### 문제 링크
-> https://www.acmicpc.net/problem/
+> https://www.acmicpc.net/problem/11403
 
 ### 알고리즘 분류
->- 
+>- 그래프 이론
+>- 플로이드-와샬
 
 ### 풀이 시간
->- ⏱ 분
+>- ⏱ 5분
 
 ### 해결
-> 1. 
+> 1. 플로이드 와샬 한 수 배워갑니다^^
