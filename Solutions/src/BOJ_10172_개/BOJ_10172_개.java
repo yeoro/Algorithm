772e896dbc872bb2894669eeeb30e3103e4095f1
yeoro <dufgh1009@naver.com>
2021-03-07 23:19:21 +0900

    20210307 | BOJ 10172 개

---
+++ Solutions/src/BOJ_10172_개/BOJ_10172_개.java
@@ -0,0 +1,17 @@
+package BOJ_10172_°³;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_10172_°³ {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
+
---
+++ Solutions/src/BOJ_10172_개/README.md
@@ -0,0 +1,20 @@
+# <img src="https://static.solved.ac/tier_small/1.svg" width=30> 10172: 개
+
+### 문제 링크
+> https://www.acmicpc.net/problem/10172
+
+### 알고리즘 분류
+>- 구현
+
+### 풀이 시간
+>- ⏱ 1분
+
+### 해결
+> ![good](../../../Img/good.png)
+>- 특수문자 입출력
+>   |입력|출력|
+>   |:----:|:----:|
+>   |\\\\ | \ |
+>   |\\' | ' |
+>   |\\" | " |
+
