9d967e532146e077cc323deedababddbbc43df1d
yeoro <dufgh1009@naver.com>
2021-05-04 23:55:42 +0900

    20210504 | PG 2017 카카오코드 본선 단체사진 찍기

--- Solutions/src/PG_2017카카오코드본선_단체사진찍기/Solution.java
+++ Solutions/src/PG_2017카카오코드본선_단체사진찍기/Solution.java
@@ -5,88 +5,81 @@ import java.util.*;
 
 class Solution {
 	
-	static ArrayList<String> list;
-	static String[] friends;
+	static HashMap<String, Integer> hm = new HashMap<String, Integer>();
+    static int[] position;
+    static String[] datas;
 	static boolean[] v;
-	static int N, answer;
+	static int answer;
 	
 	public static void main(String[] args) {
-		int n = 2;
 		String[] data = {"N~F=0", "R~T>2"};
-		String[] friends = {"A", "C", "F", "J", "M", "N", "R", "T"};
 		
-		list = new ArrayList<String>();
+		datas = data;
 		answer = 0;
-		N = friends.length;
-		v = new boolean[N];
+		v = new boolean[8];
+		position = new int[8];
 		
-		for(int i = 0; i < N; i++) {
-			v[i] = false;
-		}
+		hm.put("A", 0);
+		hm.put("C", 1);
+		hm.put("F", 2);
+		hm.put("J", 3);
+		hm.put("M", 4);
+		hm.put("N", 5);
+		hm.put("R", 6);
+		hm.put("T", 7);
 		
-		stand(0, data, friends, n);
-		
+		stand(0);
 		
 		System.out.println(answer);
 	}
 	
-	private static void stand(int depth, String[] data, String[] friends, int n) {
-		if(depth == N) {
+	private static void stand(int depth) {
+		if(depth == 8) {
 			
-			int check = 0;
-			
-			for(String s : data) {
-				
-				int n1 = list.indexOf(s.charAt(0)+"");
-				int n2 = list.indexOf(s.charAt(2)+"");
-				char op = s.charAt(3);
-				int dis = Integer.parseInt(s.charAt(4)+"");
-				
-				if(check(n1, n2, op, dis)) {
-					check++;
-				}
-			}
-			
-			if(check == n) {
+			if(check()) {
 				answer++;
 			}
 			
 			return;
 		}
 		
-		for(int i = 0; i < N; i++) {
+		for(int i = 0; i < 8; i++) {
 			if(!v[i]) {
 				v[i] = true;
-				list.add(friends[i]);
-				stand(depth+1, data, friends, n);
+				position[depth] = i;
+				stand(depth+1);
 				v[i] = false;
-				list.remove(list.size()-1);
 			}
 		}
 	}
 	
-	private static boolean check(int n1, int n2, char op, int dis) {
-		boolean res = false;
-		
-		switch(op) {
-		case '=':
-			if(Math.abs(n1 - n2) == 1) {
-				res = true;
+	private static boolean check() {
+		for(String s : datas) {
+			
+			int n1 = position[hm.get(s.charAt(0)+"")];
+			int n2 = position[hm.get(s.charAt(2)+"")];
+			char op = s.charAt(3);
+			int dis = s.charAt(4) - '0';
+			
+			switch(op) {
+			case '=':
+				if(Math.abs(n1 - n2)-1 != dis) {
+					return false;
+				}
+				break;
+			case '>':
+				if(Math.abs(n1 - n2)-1 <= dis) {
+					return false;
+				}
+				break;
+			case '<':
+				if(Math.abs(n1 - n2)-1 >= dis) {
+					return false;
+				}
+				break;
 			}
-			break;
-		case '>':
-			if(Math.abs(n1 - n2)-1 > dis) {
-				res = true;
-			}
-			break;
-		case '<':
-			if(Math.abs(n1 - n2)-1 < dis) {
-				res = true;
-			}
-			break;
 		}
 		
-		return res;
+		return true;
 	}
 }
-
