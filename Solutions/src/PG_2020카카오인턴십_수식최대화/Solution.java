ce56e70806dca10d4fab34495c102e832f097122
yeoro <dufgh1009@naver.com>
2021-05-04 17:07:37 +0900

    20210504 | PG 2020 카카오 인턴십 수식 최대화 (미완)

--- Solutions/src/PG_2020카카오인턴십_수식최대화/Solution.java
+++ Solutions/src/PG_2020카카오인턴십_수식최대화/Solution.java
@@ -7,68 +7,87 @@ class Solution {
 	
 	static ArrayList<String> list = new ArrayList<String>();
 	static ArrayList<Integer> comb = new ArrayList<Integer>();
-	static boolean[] v;
+	static ArrayList<String> num = new ArrayList<String>();
+	static String input = "100-200*300-500+20";
 	static int N;
+	static long max = Long.MIN_VALUE;
 	static StringBuilder sb = new StringBuilder("100-200*300-500+20");
 	
 	public static void main(String[] args) {
 		
-		if(sb.toString().contains("-")) {
+		if(input.contains("-")) {
 			list.add("-");
 		}
-		if(sb.toString().contains("+")) {
+		if(input.contains("+")) {
 			list.add("+");
 		}
-		if(sb.toString().contains("*")) {
+		if(input.contains("*")) {
 			list.add("*");
 		}
 		
 		N = list.size();
-		v = new boolean[list.size()];
-		
-		System.out.println(sb.indexOf("-"));
 		
 		go(0);
 		
+		System.out.println(max);
 	}
 	
 	private static void go(int depth) {
 		if(depth == N) {
-			StringBuilder r = new StringBuilder();
-			StringBuilder l = new StringBuilder();
+			System.out.println("---------------------");
 			
+			StringBuilder temp = new StringBuilder(input);
 			
 			for(int i : comb) {
-				int rIdx = sb.indexOf(list.get(i));
-				int lIdx = sb.indexOf(list.get(i));
+				String cur = list.get(i);
 				
-				// 부호 왼쪽 숫자
-				while(lIdx > 0) {
-					lIdx--;
-					
-					char cur = sb.charAt(lIdx);
-					if(cur >= '0' && cur <= '9') {
-						l.append(cur);
-					} else {
-						break;
+				for(int j = 0; j < temp.length(); j++) {
+					if((temp.charAt(j)+"").equals(cur)) {
+						StringBuilder r = new StringBuilder();
+						StringBuilder l = new StringBuilder();
+						
+						int rIdx = j+1;
+						int lIdx = j-1;
+						
+						while(lIdx >= 0) {
+							char c = temp.charAt(lIdx);
+							if(c >= '0' && c <= '9') {
+								l.append(c);
+							} else {
+								break;
+							}
+							
+							lIdx--;
+						}
+						
+						while(rIdx < temp.length()) {
+							char c = temp.charAt(rIdx);
+							if(c >= '0' && c <= '9') {
+								r.append(c);
+							} else {
+								break;
+							}
+							
+							rIdx++;
+						}
+						
+						lIdx++;
+						
+						long res = cal(cur, l.reverse().toString(), r.toString());
+						
+						
+						temp = temp.delete(lIdx, rIdx);
+						temp = temp.insert(lIdx, Long.toString(res));
+						
+						System.out.println(lIdx + " " + rIdx + " " + res + " " + cur);
+						System.out.println(l.toString() + " " + r.toString());
+						System.out.println(temp.toString());
 					}
 				}
-				
-				// 부호 오른쪽 숫자
-				while(rIdx < sb.length()-1) {
-					rIdx++;
-					
-					char cur = sb.charAt(rIdx);
-					if(cur >= '0' && cur <= '9') {
-						r.append(cur);
-					} else {
-						break;
-					}
-				}
-				
-				System.out.println(lIdx + " " + rIdx);
-				System.out.println(l.reverse().toString() + " " + r.toString());
 			}
+			
+			max = Math.max(max, Long.parseLong(temp.toString()));
+			System.out.println("---------------------");
 			
 			return;
 		}
@@ -80,6 +99,26 @@ class Solution {
 				comb.remove(comb.size()-1);
 			}
 		}
+	}
+	
+	private static long cal(String op, String l, String r) {
+		long n1 = Long.parseLong(l);
+		long n2 = Long.parseLong(r);
+		long res = 0;
+		
+		switch(op) {
+		case "*":
+			res = n1 * n2;
+			break;
+		case "+":
+			res = n1 + n2;
+			break;
+		case "-":
+			res = n1 - n2;
+			break;
+		}
+		
+		return res;
 	}
 }
 
