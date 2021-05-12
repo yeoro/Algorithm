59203781d6d6319bceef9844d42eae429f8cbdb3
yeoro <dufgh1009@naver.com>
2021-05-12 22:43:38 +0900

    20210512 | BOJ

---
+++ Solutions/src/BOJ_21610_마법사상어와비바라기/Main.java
@@ -0,0 +1,173 @@
+package BOJ_21610_마법사상어와비바라기;
+
+import java.io.*;
+import java.util.*;
+
+// 1시간 7분
+class Main {
+	
+	static class Node {
+		int x, y;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y + "]";
+		}
+	}
+	static Queue<Node> cloud = new LinkedList<Node>();
+	static int[][] map;
+	static boolean[][] v;
+	static Node[] movements;
+	static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1}, dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int N, M, res;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		
+		movements = new Node[M];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			movements[i] = new Node(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+		}
+		
+		cloud.offer(new Node(N-1, 0));
+		cloud.offer(new Node(N-1, 1));
+		cloud.offer(new Node(N-2, 0));
+		cloud.offer(new Node(N-2, 1));
+		
+		solve();
+		
+		System.out.println(res);
+		
+		br.close();
+	}
+	
+	private static void solve() {
+		for(int i = 0; i < M; i++) {
+			v = new boolean[N][N];
+			
+			move(i);
+			
+//			System.out.println("------이동");
+//			for(Node n : cloud) {
+//				System.out.println(n.toString());
+//			}
+			
+			copy();
+			
+//			System.out.println("------물복사");
+//			for(int k = 0; k < N; k++) {
+//				for(int j = 0; j < N; j++) {
+//					System.out.print(map[k][j] + " ");
+//				}
+//				System.out.println();
+//			}
+			
+			makeCloud();
+			
+//			System.out.println("-------구름생성");
+//			for(int k = 0; k < N; k++) {
+//				for(int j = 0; j < N; j++) {
+//					System.out.print(map[k][j] + " ");
+//				}
+//				System.out.println();
+//			}
+//			
+//			for(Node n : cloud) {
+//				System.out.println(n.toString());
+//			}
+			
+		}
+		
+		count();
+	}
+	
+	private static void move(int idx) {
+		Node curMove = movements[idx];
+		int d = curMove.x;
+		int s = curMove.y;
+		
+		for(int i = 0, size = cloud.size(); i < size; i++) {
+			Node cur = cloud.poll();
+			int x = cur.x;
+			int y = cur.y;
+			
+			int nx = (x + dx[d]*s) % N;
+			int ny = (y + dy[d]*s) % N;
+			
+			if(nx < 0) nx += N;
+			if(ny < 0) ny += N;
+			
+			map[nx][ny]++;
+			cloud.offer(new Node(nx, ny));
+		}
+	}
+	
+	private static void copy() {
+		while(!cloud.isEmpty()) {
+			Node cur = cloud.poll();
+			int x = cur.x;
+			int y = cur.y;
+			
+			int cnt = 0;
+			
+			//1, 3, 5, 7 방향
+			for(int i = 1; i < 8; i+=2) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(isIn(nx, ny) && map[nx][ny] > 0) {
+					cnt++;
+				}
+			}
+			
+			v[x][y] = true;
+			map[x][y] += cnt;
+		}
+	}
+	
+	private static void makeCloud() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(map[i][j] >= 2 && !v[i][j]) {
+					cloud.offer(new Node(i, j));
+					map[i][j] -= 2;
+				}
+			}
+		}
+	}
+	
+	private static void count() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				res += map[i][j];
+			}
+		}
+	}
+	
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
+}
+
---
+++ Solutions/src/BOJ_21611_마법사상어와블리자드/Main.java
@@ -0,0 +1,209 @@
+package BOJ_21611_마법사상어와블리자드;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	static class Node {
+		int x, y;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y +  "]";
+		}
+	}
+	static LinkedList<Node> list = new LinkedList<Node>();
+	static Node[] spell;
+	static Node[] bead;
+	static int[][] map;
+	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+	static int N, M, end, one, two, three;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		spell = new Node[M];
+		bead = new Node[N*N];
+		end = N*N;
+
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			spell[i] = new Node(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+		}
+		
+		bead[0] = new Node(N/2, N/2);
+		
+		int x = 0; int y = N-1;
+		for(int i = 0; i < N/2; i++) {
+			for(int j = i; j < y; j++) {
+				list.addFirst(new Node(x, j));
+			}
+			
+			for(int j = i; j < y; j++) {
+				list.addFirst(new Node(j, y));
+			}
+			
+			for(int j = y; j > i; j--) {
+				list.addFirst(new Node(y, j));
+			}
+			
+			for(int j = y; j > i; j--) {
+				list.addFirst(new Node(j, x));
+			}
+			
+			x++; y--;
+		}
+		list.addFirst(new Node(N/2, N/2));
+		
+		for(Node n : list) {
+			System.out.println(n.toString());
+		}
+		
+		solve();
+		
+		br.close();
+	}
+	
+	private static void solve() {
+		for(int i = 0; i < M; i++) {
+			cast(i);
+			
+			System.out.println("-----마법시전");
+			for(int k = 0; k < N; k++) {
+				for(int j = 0; j < N; j++) {
+					System.out.print(map[k][j] + " ");
+				}
+				System.out.println();
+			}
+			
+			move();
+			
+			System.out.println("-----빈자리이동");
+			for(int k = 0; k < N; k++) {
+				for(int j = 0; j < N; j++) {
+					System.out.print(map[k][j] + " ");
+				}
+				System.out.println();
+			}
+			
+			if(check()) {
+				bomb();
+			}
+			
+		}
+	}
+	
+	private static void cast(int idx) {
+		Node curSpell = spell[idx];
+		int d = curSpell.x; int s = curSpell.y;
+		
+		for(int i = 1; i <= s; i++) {
+			int nx = N/2 + dx[d]*i;
+			int ny = N/2 + dy[d]*i;
+			
+			switch(map[nx][ny]) {
+			case 1:
+				one++;
+				break;
+			case 2:
+				two++;
+				break;
+			case 3:
+				three++;
+				break;
+			}
+			
+			map[nx][ny] = 0;
+		}
+	}
+	
+	private static void move() {
+		loop:for(int i = 1, size = list.size(); i < size; i++) {
+			Node cur = bead[i];
+			int x = cur.x; int y = cur.y;
+			
+			if(map[x][y] == 0) {
+				for(int j = i+1; j < size; j++) {
+					Node next = bead[j];
+					int nx = next.x; int ny = next.y;
+					
+					if(map[nx][ny] != 0) {
+						map[x][y] = map[nx][ny];
+						map[nx][ny] = 0;
+						
+						continue loop;
+					}
+				}
+			}
+		}
+	}
+	
+	private static boolean check() {
+		int cnt = 1;
+		
+		for(int i = 1; i < list.size()-2; i++) {
+			Node cur = list.get(i);
+			int x = cur.x; int y = cur.y;
+			
+			Node next = list.get(i+1);
+			int nx = next.x; int ny = next.y;
+			
+			if(map[x][y] == map[nx][ny]) {
+				cnt++;
+				if(cnt >= 4) {
+					return true;
+				}
+			} else {
+				cnt = 1;
+			}
+		}
+		
+		return false;
+	}
+	
+	private static void bomb() {
+		ArrayList<Node> temp = new ArrayList<Node>();
+		
+		for(int i = 1; i < list.size()-2; i++) {
+			temp.add(list.get(i));
+			
+			Node cur = list.get(i);
+			int x = cur.x; int y = cur.y;
+			
+			Node next = list.get(i+1);
+			int nx = next.x; int ny = next.y;
+			
+			if(map[x][y] == map[nx][ny]) {
+				temp.add(list.get(i+1));
+			} else {
+				temp.remove(list.get(list.size()-1));
+			}
+		}
+		
+		System.out.println("-----연속");
+		for(Node n : temp) {
+			System.out.println(n.toString());
+		}
+	}
+}
+
