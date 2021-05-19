382ef86bb982345ada41f0d83aa988915efdd3e3
yeoro <dufgh1009@naver.com>
2021-05-19 22:05:50 +0900

    20210518 | BOJ 10942 팰린드롬? (시간 초과)

---
+++ Solutions/src/BOJ_10942_팰린드롬/Main.java
@@ -0,0 +1,64 @@
+package BOJ_10942_팰린드롬;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	static int[][] dp;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 1; i <= N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp = new int[N+1][N+1];
+		
+		// 길이가 1인 수
+		for(int i = 1; i <= N; i++) {
+			dp[i][i] = 1;
+		}
+		
+		// 길이가 2인 수
+		for(int i = 1; i < N; i++) {
+			if(nums[i] == nums[i+1]) {
+				dp[i][i+1] = 1;
+			}
+		}
+		
+		// 길이가 3이상인 수
+		for(int i = 2; i <= N; i++) {
+			for(int j = 1; j < N-1; j++) {
+				if(nums[j] == nums[j+i] && dp[j+1][]) {
+					
+				}
+			}
+		}
+		
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			bw.write(dp[start][end] + "\n");
+		}
+		
+		bw.flush();
+		br.close();
+	}
+}
+
---
+++ Solutions/src/BOJ_10942_팰린드롬/README.md
@@ -0,0 +1,19 @@
+# <img src="https://static.solved.ac/tier_small/31.svg" width=30> 00000: 
+
+### 문제 링크
+> https://www.acmicpc.net/problem/
+
+### 알고리즘 분류
+>- 
+
+### 풀이 시간
+>- ⏱ 분
+
+### 해결
+> ![bad](../../../Img/bad.png)  
+>1. 1
+>2. 2
+>  
+> ![good](../../../Img/good.png)
+>1. 1
+>2. 2
