a9f0cf21dc97da8c863b8f876d0e6e97c4038ab8
yeoro <dufgh1009@naver.com>
2021-05-23 16:11:19 +0900

    20210523 | BOJ 11053 가장 긴 증가하는 부분 수열 O(N^2)

---
+++ Solutions/src/BOJ_11053_가장긴증가하는부분수열/Main.java
@@ -0,0 +1,40 @@
+package BOJ_11053_가장긴증가하는부분수열;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+
+		int[] nums = new int[N];
+		int[] dp = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i < N; i++) {
+			dp[i] = 1;
+			
+			for(int j = 0; j < i; j++) {
+				if(nums[j] < nums[i]) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
+			}
+		}
+		
+		Arrays.sort(dp);
+		
+		System.out.println(dp[N-1]);
+		
+		br.close();
+	}
+}
+
