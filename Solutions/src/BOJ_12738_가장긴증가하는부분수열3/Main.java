20fe68e9ffd8855ea0e6a65a26afcb5f35ae4a94
yeoro <dufgh1009@naver.com>
2021-05-25 00:32:49 +0900

    20210524 | BOJ 12738 가장 긴 증가하는 부분 수열 3

---
+++ Solutions/src/BOJ_12738_가장긴증가하는부분수열3/Main.java
@@ -0,0 +1,57 @@
+package BOJ_12738_가장긴증가하는부분수열3;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	static int[] dp, nums;
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N];
+		nums = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0] = nums[0];
+		int dIdx = 0;
+		
+		for(int nIdx = 1; nIdx < N; nIdx++) {
+			if(dp[dIdx] < nums[nIdx]) {
+				dp[dIdx+1] = nums[nIdx];
+				dIdx++;
+			} else {
+				int idx = binarySearch(0, dIdx, nums[nIdx]);
+				dp[idx] = nums[nIdx];
+			}
+		}
+		
+		System.out.println(dIdx+1);
+		
+		br.close();
+	}
+	
+	private static int binarySearch(int left, int right, int target) {
+		while(left < right) {
+			int mid = (left + right) / 2;
+			
+			if(dp[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		
+		return right;
+	}
+}
+
---
+++ Solutions/src/BOJ_12738_가장긴증가하는부분수열3/README.md
@@ -0,0 +1,15 @@
+# <img src="https://static.solved.ac/tier_small/14.svg" width=30> 12738: 가장 긴 증가하는 부분 수열 3 
+
+### 문제 링크
+> https://www.acmicpc.net/problem/12738
+
+### 알고리즘 분류
+>- 이분 탐색
+>- 가장 긴 증가하는 부분 수열: O(n log n)
+
+### 풀이 시간
+>- ⏱ 3분
+
+### 해결
+> ![good](../../../Img/good.png)
+>1. [가장 긴 증가하는 부분 수열 2](../BOJ_12015_가장긴증가하는부분수열2) 와 뭐가 다른거지..?
