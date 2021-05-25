a1c97f13802f0e33256a964b7727650e90537559
yeoro <dufgh1009@naver.com>
2021-05-25 18:23:26 +0900

    20210525 | BOJ 11053 가장 긴 증가하는 부분 수열 (추가)

---
+++ Solutions/src/BOJ_09663_NQueen/Main.java
@@ -0,0 +1,16 @@
+package BOJ_09663_NQueen;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	static int[][] board;
+	static boolean[][] v;
+	static int N;
+	
+	public static void main(String[] args) {
+		
+	}
+}
+
